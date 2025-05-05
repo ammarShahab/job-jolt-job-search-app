0512ffebd015433224e23c816ae1ab3761290943
Ashahab007 <ashahab007@gmail.com>
2025-05-06 01:21:09 +0600

    hide tha firebase data to env

--- public/firebase/firebase.config.js
+++ public/firebase/firebase.config.js
@@ -1,14 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRdRBDkZUTjSmcpHlpCVX9E684NTBWdVE",
-  authDomain: "b11a9-react-authenticati-66750.firebaseapp.com",
-  projectId: "b11a9-react-authenticati-66750",
-  storageBucket: "b11a9-react-authenticati-66750.firebasestorage.app",
-  messagingSenderId: "54707990143",
-  appId: "1:54707990143:web:1f68e6910f41867891994a",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
