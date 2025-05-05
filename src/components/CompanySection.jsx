2e02adf37b5aa12d912f3d987bc7f819eed543b4
Ashahab007 <ashahab007@gmail.com>
2025-05-05 14:50:41 +0600

    created how it works section

--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 const CompanySection = () => {
   return (
     <div>
       <h3>Trusted by more than 100+ Companies</h3>
+      <div>
+        <NavLink></NavLink>
+      </div>
     </div>
   );
 };
