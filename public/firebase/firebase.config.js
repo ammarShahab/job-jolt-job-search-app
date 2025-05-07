4ceb9acbb5b3d735cdb6f9c8126317a5daa1268d
Ashahab007 <ashahab007@gmail.com>
2025-05-07 21:26:53 +0600

    deploy to netlify

--- public/firebase/firebase.config.js
+++ public/firebase/firebase.config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
-}; */
+};
 
 // .env.local
 // VITE_apiKey=AIzaSyDRdRBDkZUTjSmcpHlpCVX9E684NTBWdVE,
@@ -18,14 +18,14 @@ import { getAuth } from "firebase/auth";
 // VITE_messagingSenderId=54707990143,
 // VITE_appId=1:54707990143:web:1f68e6910f41867891994a,
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyDRdRBDkZUTjSmcpHlpCVX9E684NTBWdVE",
   authDomain: "b11a9-react-authenticati-66750.firebaseapp.com",
   projectId: "b11a9-react-authenticati-66750",
   storageBucket: "b11a9-react-authenticati-66750.firebasestorage.app",
   messagingSenderId: "54707990143",
   appId: "1:54707990143:web:1f68e6910f41867891994a",
-};
+}; */
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -2,7 +2,9 @@ import React, { use } from "react";
 import { Link, NavLink } from "react-router";
 import { motion } from "motion/react";
 
-const companiesPromise = fetch("/public/jobs.json").then((res) => res.json());
+const companiesPromise = fetch("/public/jobs.json")
+  .then((res) => res.json())
+  .catch((err) => console.error("Fetch error:", err));
 console.log(companiesPromise);
 
 const AnimatedLink = motion(Link);
@@ -18,19 +20,19 @@ const CompanySection = () => {
       </h3>
 
       <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-16">
-        {companies.map((company) => (
+        {companies?.map((company) => (
           // Applying react motion animation
           <>
             <AnimatedLink
               whileHover={{ scale: 1.2 }}
               whileTap={{ scale: 0.8 }}
-              to={`/company-details/${company.id}`}
-              key={company.id}
+              to={`/company-details/${company?.id}`}
+              key={company?.id}
               className="flex items-center justify-center p-4 rounded-xl bg-gray-50 shadow-2xl"
             >
               <img
-                src={company.logo}
-                alt={company.name}
+                src={company?.logo}
+                alt={company?.name}
                 className="h-16 object-contain"
               />
             </AnimatedLink>
