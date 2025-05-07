8acea44bf0e7d81205dbc7a6a8ee043a9160fb80
Ashahab007 <ashahab007@gmail.com>
2025-05-07 18:57:52 +0600

    applied react motion in company sectuion

--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -1,8 +1,11 @@
 import React, { use } from "react";
 import { Link, NavLink } from "react-router";
+import { motion } from "motion/react";
 
 const companiesPromise = fetch("/public/jobs.json").then((res) => res.json());
 console.log(companiesPromise);
+
+const AnimatedLink = motion(Link);
 
 const CompanySection = () => {
   const companies = use(companiesPromise);
@@ -13,19 +16,25 @@ const CompanySection = () => {
       <h3 className="text-center font-extrabold text-5xl mt-20">
         Trusted by more than 100+ Companies
       </h3>
+
       <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-16">
         {companies.map((company) => (
-          <Link
-            to={`/company-details/${company.id}`}
-            key={company.id}
-            className="flex items-center justify-center p-4 rounded-xl bg-gray-50 hover:shadow-2xl transition"
-          >
-            <img
-              src={company.logo}
-              alt={company.name}
-              className="h-16 object-contain"
-            />
-          </Link>
+          // Applying react motion animation
+          <>
+            <AnimatedLink
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+              to={`/company-details/${company.id}`}
+              key={company.id}
+              className="flex items-center justify-center p-4 rounded-xl bg-gray-50 shadow-2xl"
+            >
+              <img
+                src={company.logo}
+                alt={company.name}
+                className="h-16 object-contain"
+              />
+            </AnimatedLink>
+          </>
         ))}
       </div>
     </div>
